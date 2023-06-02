4fea3eb751640c63eb150ff8977a3fbea9ff8c68
industry1111 <industry1111@naver.com>
2023-06-03 00:08:30 +0900

    chapter 2

---
+++ src/main/java/com/example/book/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.book.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
